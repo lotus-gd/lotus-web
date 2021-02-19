bdfbd9fed70452985d8eae2b9d8663cd2093980e
SpookyBear0 <50897638+SpookyBear0@users.noreply.github.com>
2021-02-19 12:05:13 -0800

    fix index

--- router.py
+++ router.py
@@ -1,10 +1,11 @@
 import aiohttp
 import importlib
 
-routes = ["index", "gaming/gamer"]
+routes = ["index"]
 
 def add_all_routes(app: aiohttp.web.Application):
     for route in routes:
         modulepath = route.replace("/", ".")
         routesplit = route.split("/")
+        if route == "index": route = ""
         app.router.add_get("/" + route, getattr(importlib.import_module("handlers." + modulepath), routesplit[-1]))
