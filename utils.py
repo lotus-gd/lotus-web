76b74d42b20ec370716483f0ef06614676412aff
Noah <altenyt@gmail.com>
2021-02-22 15:09:01 -0500

    Add navbar and initial base layout still need to write some js to make the "search" function work

--- utils.py
+++ utils.py
@@ -2,6 +2,9 @@ import aiohttp
 from web import templates
 
 def render_template(template, **kwargs) -> aiohttp.web.Response:
+    if not template.endswith(".html"):
+        template += ".html"  # make sure we have .html always at the end or else the server will break.
+
     text = templates.get_template(template).render(**kwargs)
     return aiohttp.web.Response(text=text, content_type="text/html")
 
