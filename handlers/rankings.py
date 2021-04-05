970ed95e7ff3092dec846d4c6f8d033cfea1d9aa
SpookyBear0 <50897638+spookybear0@users.noreply.github.com>
2021-04-05 02:50:13 -0700

    add auto sort

--- handlers/rankings.py
+++ handlers/rankings.py
@@ -1,7 +1,12 @@
 import aiohttp
 import utils
 from common.objects import Account
+import operator
 
 async def rankings(r: aiohttp.web.RequestHandler):
-    scores = [Account(username="spookybear0", pp=10000), Account(username="RealistikDash", pp=1000), Account(username="Laica", pp=100)]
+    scores = [Account(username="spookybear0", pp=1), Account(username="RealistikDash", pp=1000), Account(username="Laica", pp=100)]
+    
+    scores.sort(key=operator.attrgetter("pp"))
+    scores.reverse()
+    
     return utils.render_template("rankings.html", leaderboard_scores=scores)
