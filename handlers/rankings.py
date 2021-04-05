339c0eba5d111f6edac7e98d2b617231c50bfdb0
SpookyBear0 <50897638+spookybear0@users.noreply.github.com>
2021-04-05 15:58:04 -0700

    add some stuff to rankings

--- handlers/rankings.py
+++ handlers/rankings.py
@@ -1,10 +1,10 @@
 import aiohttp
 import utils
-from common.objects import Account
 import operator
+from common.objects import Account
 
 async def rankings(r: aiohttp.web.RequestHandler):
-    scores = [Account(username="spookybear0", pp=1), Account(username="RealistikDash", pp=1000), Account(username="Laica", pp=100)]
+    scores = [Account(username="spookybear0", pp=1), Account(username="RealistikDash", pp=1000), Account(username="Laica", pp=100), Account(username="Electro", pp=2001)]
     
     scores.sort(key=operator.attrgetter("pp"))
     scores.reverse()
