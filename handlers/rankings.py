c2dc745f44aa155807cbef9a040838dddb3e5ecd
SpookyBear0 <50897638+spookybear0@users.noreply.github.com>
2021-04-05 02:48:13 -0700

    leaderboard is pog

--- handlers/rankings.py
+++ handlers/rankings.py
@@ -1,5 +1,7 @@
 import aiohttp
 import utils
+from common.objects import Account
 
 async def rankings(r: aiohttp.web.RequestHandler):
-    return utils.render_template("rankings.html")
+    scores = [Account(username="spookybear0", pp=10000), Account(username="RealistikDash", pp=1000), Account(username="Laica", pp=100)]
+    return utils.render_template("rankings.html", leaderboard_scores=scores)
