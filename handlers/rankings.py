e212d1c5b0e09e68b80b6afe18b471c1d2749b90
SpookyBear0 <50897638+spookybear0@users.noreply.github.com>
2021-04-08 22:06:04 -0700

    sort pp correctly

--- handlers/rankings.py
+++ handlers/rankings.py
@@ -6,7 +6,7 @@ from common import Account, leaderboardhelper, userhelper
 async def rankings(r: aiohttp.web.RequestHandler):
     #await leaderboardhelper.refresh_leaderboards() # broken rn
     scores = list(await leaderboardhelper.get_top_100())
-    scores.sort()
+    scores.sort(key=operator.attrgetter(scores[0]))
     rankings = []
     # we need to get the username and the pp (its just the id rn)
     for score in scores:
