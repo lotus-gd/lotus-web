8d18a3fceb08645a6d1a67b005f0ff2cb515dd82
SpookyBear0 <50897638+spookybear0@users.noreply.github.com>
2021-04-08 20:23:10 -0700

    replace text with not available because we need to get the stuff from db but im tired and i want to go to bed

--- handlers/rankings.py
+++ handlers/rankings.py
@@ -9,7 +9,7 @@ async def rankings(r: aiohttp.web.RequestHandler):
     scores = list(await leaderboard.get_top_100())
     scores.sort()
     rankings = []
-    # we need to get the username (its just the id rn)
+    # we need to get the username and the pp (its just the id rn)
     for score in scores:
-        rankings.append(Account(username=score[1], pp=score[0]))
+        rankings.append(Account(username="not available", pp="not available"))
     return utils.render_template("rankings.html", leaderboard_scores=rankings)
