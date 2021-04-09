46044b97b1f5023e768ea6fc958af3edef2512be
SpookyBear0 <50897638+spookybear0@users.noreply.github.com>
2021-04-08 21:50:27 -0700

    i need to await

--- handlers/rankings.py
+++ handlers/rankings.py
@@ -10,7 +10,7 @@ async def rankings(r: aiohttp.web.RequestHandler):
     rankings = []
     # we need to get the username and the pp (its just the id rn)
     for score in scores:
-        print(score[0])
-        user = userhelper.get_user(score[0])
+        print(score[1])
+        user = await userhelper.get_user(score[1])
         rankings.append(Account(username=user.username, pp=user.pp))
     return utils.render_template("rankings.html", leaderboard_scores=rankings)
