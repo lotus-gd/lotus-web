ea0978dd771c50e82d33770eb4b6c3526589d8a1
SpookyBear0 <50897638+spookybear0@users.noreply.github.com>
2021-04-08 22:08:15 -0700

    ACTUALLY FIX IT :trollface:

--- handlers/rankings.py
+++ handlers/rankings.py
@@ -1,12 +1,11 @@
 import aiohttp
 import utils
-import operator
 from common import Account, leaderboardhelper, userhelper
 
 async def rankings(r: aiohttp.web.RequestHandler):
     #await leaderboardhelper.refresh_leaderboards() # broken rn
     scores = list(await leaderboardhelper.get_top_100())
-    scores.sort(key=operator.attrgetter(scores[0]))
+    scores.sort(reverse=True)
     rankings = []
     # we need to get the username and the pp (its just the id rn)
     for score in scores:
