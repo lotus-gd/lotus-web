eafdb6a9838b58a3c3b04652a6cd90a42508a4a9
SpookyBear0 <50897638+spookybear0@users.noreply.github.com>
2021-04-09 12:00:52 -0700

    sort by pp

--- handlers/rankings.py
+++ handlers/rankings.py
@@ -3,12 +3,9 @@ import utils
 from common import Account, leaderboardhelper, userhelper
 
 async def rankings(r: aiohttp.web.RequestHandler):
-    #await leaderboardhelper.refresh_leaderboards() # broken rn
-    scores = list(await leaderboardhelper.get_top_100())
-    scores.sort(reverse=True)
+    scores = await leaderboardhelper.get_top_100()
     rankings = []
     # we need to get the username and the pp (its just the id rn)
     for score in scores:
-        user = await userhelper.get_user(score[1])
-        rankings.append(Account(username=user.username, pp=user.pp))
+        rankings.append(Account(username=score[0], pp=score[1]))
     return utils.render_template("rankings.html", leaderboard_scores=rankings)
