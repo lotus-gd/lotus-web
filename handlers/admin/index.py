df0f75267fcba0515d04a5e6b2aee7041dbde682
SpookyBear0 <50897638+spookybear0@users.noreply.github.com>
2021-05-26 22:32:15 -0700

    admin panel

--- handlers/admin/index.py
+++ handlers/admin/index.py
@@ -1,5 +1,12 @@
 import aiohttp
-import utils
+import utils # type: ignore
+from aiohttp_session import get_session
+from common.helpers import userhelper # type: ignore
+from common.constants import Privileges # type: ignore
 
 async def index(r: aiohttp.web.RequestHandler):
-    return await utils.render_template(r, "admin.html")
+    session = await get_session(r)
+    if not session["user_id"]: return aiohttp.web.HTTPForbidden()
+    #user = await userhelper.get_user(session["user_id"])
+    #if not user.privilege_group.has_privilege(Privileges.DEVELOPER): return aiohttp.web.HTTPForbidden() # check if admin here
+    return await utils.render_template(r, "admin.html", totalusers=await userhelper.get_total_users(), users=await userhelper.get_all_users())
