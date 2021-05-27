76eede8a329b4dd39128042638f39db7d5f481b6
SpookyBear0 <50897638+spookybear0@users.noreply.github.com>
2021-05-26 22:35:13 -0700

    exception handling

--- handlers/admin/index.py
+++ handlers/admin/index.py
@@ -6,7 +6,10 @@ from common.constants import Privileges # type: ignore
 
 async def index(r: aiohttp.web.RequestHandler):
     session = await get_session(r)
-    if not session["user_id"]: return aiohttp.web.HTTPForbidden()
+    try:
+        if not session["user_id"]: return aiohttp.web.HTTPForbidden()
+    except KeyError:
+        return aiohttp.web.HTTPForbidden()
     #user = await userhelper.get_user(session["user_id"])
     #if not user.privilege_group.has_privilege(Privileges.DEVELOPER): return aiohttp.web.HTTPForbidden() # check if admin here
     return await utils.render_template(r, "admin.html", totalusers=await userhelper.get_total_users(), users=await userhelper.get_all_users())
