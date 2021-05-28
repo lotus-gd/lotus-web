6fee411839b5f81056ff85129178484627f6aacb
SpookyBear0 <50897638+spookybear0@users.noreply.github.com>
2021-05-27 22:52:48 -0700

    change to stars for leaderboard

--- handlers/rankings.py
+++ handlers/rankings.py
@@ -3,12 +3,12 @@ import utils
 from common import Account, leaderboardhelper, userhelper
 
 async def rankings(r: aiohttp.web.RequestHandler):
-    scores = await leaderboardhelper.get_top_100()
+    scores = await leaderboardhelper.get_top_100_stars()
     rankings = []
-    # we need to get the username and the pp (its just the id rn)
+    # we need to get the username and the stars
     for score in scores:
         acc = Account(-1)
         acc.name = score[0]
-        acc.pp = int(score[1])
+        acc.stats.stars = int(score[1])
         rankings.append(acc)
     return await utils.render_template(r, "rankings.html", leaderboard_scores=rankings)
