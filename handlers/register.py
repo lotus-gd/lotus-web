35e3abec74836217295026d5fae9086222548047
SpookyBear0 <50897638+spookybear0@users.noreply.github.com>
2021-05-29 23:30:23 -0700

    add referral system

--- handlers/register.py
+++ handlers/register.py
@@ -5,7 +5,7 @@ from common.objects.account import Account # type: ignore
 from common.helpers import privilegehelper # type ignore
 
 async def register(r: aiohttp.web.RequestHandler):
-    return await utils.render_template(r, "register.html")
+    return await utils.render_template(r, "register.html", referer=r.rel_url.query.get("referer", 0))
 
 async def register_post(r: aiohttp.web.RequestHandler):
     if await privilegehelper.logged_in(r):
@@ -14,6 +14,7 @@ async def register_post(r: aiohttp.web.RequestHandler):
     data = await r.post()
     username = data["username"]
     password = data["password"]
+    referer = data["referer"]
     email = data["email"]
     
     if not regex.match(".{1,}@[^.]{1,}", email):
@@ -23,6 +24,6 @@ async def register_post(r: aiohttp.web.RequestHandler):
         ip = r.headers["CF-Connecting-IP"]
     except KeyError:
         ip = "0.0.0.0"
-    await Account.register(username, password, email, ip)
+    await Account.register(username, password, email, ip, referer)
     
     return aiohttp.web.HTTPFound("/")
--- handlers/search.py
+++ handlers/search.py
@@ -2,5 +2,5 @@ import aiohttp
 import utils
 
 async def search(r: aiohttp.web.RequestHandler):
-    search = r.headers["search"] # get args of search
+    search = r.rel_url.query["q"] # get args of search
     #return await utils.render_template(r, "search.html")
