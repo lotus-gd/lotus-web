d3fdb02a335e5ec7c9caea5531fb367e3ab50204
SpookyBear0 <50897638+spookybear0@users.noreply.github.com>
2021-05-30 12:12:43 -0700

    add level databasing to main site

--- handlers/admin/index.py
+++ handlers/admin/index.py
@@ -1,10 +1,12 @@
 import aiohttp
 import utils # type: ignore
-from aiohttp_session import get_session
-from common.helpers import userhelper, privilegehelper # type: ignore
+from common.helpers import userhelper, privilegehelper, levelhelper # type: ignore
 
 async def index(r: aiohttp.web.RequestHandler):
     if not await privilegehelper.logged_in(r) or not await privilegehelper.is_admin(r): 
         return aiohttp.web.HTTPForbidden()
     
-    return await utils.render_template(r, "admin.html", totalusers=await userhelper.get_total_users(), users=await userhelper.get_all_users(6))
+    return await utils.render_template(r, "admin.html",
+                                       totalusers=await userhelper.get_total_users(),
+                                       users=await userhelper.get_all_users(6),
+                                       totallevels=await levelhelper.get_total_levels())
