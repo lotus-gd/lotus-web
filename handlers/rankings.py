ccfe17e9618d47aaa4beb0156a3de38f804598fd
SpookyBear0 <50897638+spookybear0@users.noreply.github.com>
2021-05-31 01:04:37 -0700

    add prev and next to leaderboards

--- handlers/rankings.py
+++ handlers/rankings.py
@@ -3,10 +3,11 @@ import utils # type: ignore
 from common import leaderboardhelper, userhelper # type: ignore
 
 async def rankings(r: aiohttp.web.RequestHandler):
+    page = int(r.rel_url.query.get("page", 0))
     scores = await leaderboardhelper.get_top_100_stars()
     rankings = []
     # we need to get the username and the stars
-    for score in scores:
+    for i, score in enumerate(scores[page*25:]):
         acc = await userhelper.get_user_by_name(score[0])
         rankings.append(acc)
-    return await utils.render_template(r, "rankings.html", leaderboard_scores=rankings)
+    return await utils.render_template(r, "rankings.html", leaderboard_scores=rankings, page=page)
