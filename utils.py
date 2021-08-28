43331bbc142d6ffd30115a65ae20cff6772ad7c5
SpookyBear0 <50897638+spookybear0@users.noreply.github.com>
2021-08-27 20:52:26 -0700

    only show admin button when user is an admin

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import aiohttp
 from web import templates # type: ignore
 from aiohttp_session import get_session
-from common import userhelper # type: ignore
+from common.helpers import userhelper, privilegehelper # type: ignore
 
 async def render_template(r, template, *args, **kwargs) -> aiohttp.web.Response:
     
@@ -11,9 +11,12 @@ async def render_template(r, template, *args, **kwargs) -> aiohttp.web.Response:
     
     session = await get_session(r)
     user = None
+    is_admin = False
     if session.get("user_id") and session.get("logged_in"):
         user = await userhelper.get_user(session.get("user_id"))
-    text = templates.get_template(template).render(*args, **kwargs, session=session, user=user)
+        if await privilegehelper.is_admin(r):
+            is_admin = True
+    text = templates.get_template(template).render(*args, **kwargs, session=session, user=user, is_admin=is_admin)
     return aiohttp.web.Response(text=text, content_type="text/html")
 
 def text(_text, **kwargs) -> aiohttp.web.Response:
